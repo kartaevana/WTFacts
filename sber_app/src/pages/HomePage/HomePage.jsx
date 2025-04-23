eb3722f0f6c4139781865e2df7c32e634b4fe48f
Solyanik Varvara <solyanikvarvara@MacBook-Air-Solyanik.local>
2025-04-23 18:41:59 +0300

    script

--- sber_app/src/pages/HomePage/HomePage.jsx
+++ sber_app/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,11 @@ import './HomePage.css';
 
 export const HomePage = () => {
     return (
-        <p className="home_text">HOME!</p>
-    )
+        <div className="home_container">
+            <p className="home_text">WTFacts</p>
+            <button className="wtfacts-reveal-btn" id="revealFactBtn">
+                Расскажи что-то интересное
+            </button>
+        </div>
+    );
 }
