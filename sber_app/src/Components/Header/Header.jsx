8bd5edaa74da4855dd4875c85d29ff6a13767839
kartaevana@yandex.ru <kartaevana@yandex.ru>
2025-04-23 20:42:32 +0300

    add WTF

--- sber_app/src/Components/Header/Header.jsx
+++ sber_app/src/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import './Header.css';
+import userPhoto from './wtf.png'; // Файл лежит в той же папке, что и Header.jsx
 
 export const Header = () => {
     return (
         <header className="header">
-            <div className="circle"></div>
+            <img
+                src={userPhoto}
+                alt="User"
+                className="user-photo"
+            />
         </header>
     );
 }
